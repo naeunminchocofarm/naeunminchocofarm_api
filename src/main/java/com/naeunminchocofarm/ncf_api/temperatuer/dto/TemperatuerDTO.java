9b7979ff4ab8d17cbf3dcfec519b7f29893c73d0
Lin-Bee <lin_bee@naver.com>
2025-03-26 12:05:05 +0900

    팀원 작업받기전 임시 commit

--- src/main/java/com/naeunminchocofarm/ncf_api/temperatuer/dto/TemperatuerDTO.java
+++ src/main/java/com/naeunminchocofarm/ncf_api/temperatuer/dto/TemperatuerDTO.java
@@ -5,14 +5,19 @@ import java.time.OffsetDateTime;
 public class TemperatuerDTO {
 	//날짜시간
 	//온도 (최저 최고 평균) > min max avg는 리액트
-	private final int Id;
-	private final OffsetDateTime tempDay;
+	private final OffsetDateTime measuredAt;
 	private final double temperatuer;
 
+	public OffsetDateTime getTempDay() {
+		return measuredAt;
+	}
 
-	public TemperatuerDTO(int id, OffsetDateTime tempDay, double temperatuer) {
-		Id = id;
-		this.tempDay = tempDay;
+	public double getTemperatuer() {
+		return temperatuer;
+	}
+
+	public TemperatuerDTO(OffsetDateTime measuredAt, double temperatuer) {
+		this.measuredAt = measuredAt;
 		this.temperatuer = temperatuer;
 	}
 }
