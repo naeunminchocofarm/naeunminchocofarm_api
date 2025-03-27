a20192912e7a1bc72b062a316ff7e3b9fba02800
koeyismyusername <koey2182@gmail.com>
2025-03-27 12:35:10 +0900

    LDR 센서 값 삽입 응답 204로 변경

--- src/main/java/com/naeunminchocofarm/ncf_api/ldr/controller/LdrController.java
+++ src/main/java/com/naeunminchocofarm/ncf_api/ldr/controller/LdrController.java
@@ -3,8 +3,10 @@ package com.naeunminchocofarm.ncf_api.ldr.controller;
 import com.naeunminchocofarm.ncf_api.ldr.service.LdrService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.OffsetDateTime;
@@ -19,6 +21,7 @@ public class LdrController {
     }
 
     @PostMapping("/ldr-values")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void insertLdrValue(@RequestParam("ldr-value") Integer ldrValue, @RequestParam("measured-at")OffsetDateTime measuredAt) {
         log.info("insert ldr value: value = {}, measured at = {}", ldrValue, measuredAt);
         ldrService.insertLdrValue(ldrValue, measuredAt);
