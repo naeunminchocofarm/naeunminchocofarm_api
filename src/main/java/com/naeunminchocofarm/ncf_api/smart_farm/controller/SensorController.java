f125a46789b31900cc4d720b7f4f285b4be3e5ed
koeyismyusername <koey2182@gmail.com>
2025-04-14 12:14:08 +0900

    센서 데이터 수집 로그 추가

--- src/main/java/com/naeunminchocofarm/ncf_api/smart_farm/controller/SensorController.java
+++ src/main/java/com/naeunminchocofarm/ncf_api/smart_farm/controller/SensorController.java
@@ -3,6 +3,8 @@ package com.naeunminchocofarm.ncf_api.smart_farm.controller;
 import com.naeunminchocofarm.ncf_api.smart_farm.dto.SensorDataDTO;
 import com.naeunminchocofarm.ncf_api.smart_farm.entity.Sensor;
 import com.naeunminchocofarm.ncf_api.smart_farm.service.SensorService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.List;
 @RestController
 public class SensorController {
 
+  private static final Logger log = LogManager.getLogger(SensorController.class);
   private final SensorService sensorService;
 
   public SensorController(SensorService sensorService) {
@@ -30,6 +33,7 @@ public class SensorController {
 
     @PostMapping("/sensors/datas")
     public void insertSensorDatas(@RequestBody List<SensorDataDTO> sensorDataDTOs){
+        log.info("센서 데이터 수집!");
         sensorService.insertSensorDatas(sensorDataDTOs);
     }
 }
